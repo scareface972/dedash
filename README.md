d802563fb9b4cadf6a4fc9f5a9d4e89c0c8dc431
rodobodolfo <peladovii@hotmail.com>
2016-08-16 00:15:07 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+A simple dashboard for home server and IoT projects.
+
+TO DO:
+
+* JSON Load/Save
+* Edit/View mode
+* In edit mode a code editor to paste hmtl/js code.
+* Tabs Icons
+* Font Awesome Icon Picker    https://github.com/mjolnic/fontawesome-iconpicker
+* Bootstrap-Iconpicker By Victor Valencia Rico. http://victor-valencia.github.io/bootstrap-iconpicker/#
+* Widgets 
---
+++ js/app1.js
@@ -0,0 +1,83 @@
+var app1 = angular.module('app1', ['ui.bootstrap']);
+
+app1.run(function($rootScope) {
+   $rootScope.serverName = "Test Server";
+   $rootScope.userName = "Test User";
+});
+
+
+app1.controller('ctrl1', function ($scope) {
+
+});
+
+app1.controller('navbar', function ($scope) {
+  
+
+});
+
+app1.controller('tabs', function ($scope) {
+ 
+     // It should load from JSON
+ 
+// this is the format of the JSON 
+            // rows : [
+            //     {
+            //         cols : [
+            //             {size : 6},
+            //             {size : 6}
+            //         ],
+            //     },
+            //     {
+            //         cols : [
+            //             {size : 2},
+            //             {size : 4},
+            //             {size : 6}
+            //         ]
+            //     },
+
+            // ]
+ 
+    $scope.tabs = [];
+    $scope.defaultLayout = "2 6 4";
+        
+    $scope.clickTab = function($tab) {
+        $scope.activeTab = $tab;
+        //console.log($scope.activeTab);
+    }
+
+    $scope.addTab = function() {
+        var newId = $scope.tabs.length + 1;
+        $scope.tabs.push({
+            id: newId,
+            name: "tab".concat(newId),
+            rows : []                
+        });
+    };
+
+    $scope.addRow = function($tab, $layout) {
+        //ask for user input on layout. Needs a security check. Maybe if the sum of all parts is > 12 (bootstrap grid)
+        var rowLayoutInput = $layout;
+        var rowLayout = rowLayoutInput.split(" ");
+        $tab.rows.push ({
+            cols : []
+        });                
+
+     //Iterate through each column and populate it
+     for (var x = 0; x < rowLayout.length; x++) {
+        $tab.rows[$tab.rows.length-1].cols.push({
+            size : rowLayout[x]
+        })
+     }
+       // $tab.rows[$tab.rows.length].cols[0].size = 12;
+        // for (var x = 0; x < rowLayout.length; x++) {
+        //     $tab.rows[$tab.rows.length-1].cols[x]=rowLayout[x];            
+        // }
+ 
+
+    }
+
+    $scope.renderTab = function($tab) {
+
+    };
+
+});
